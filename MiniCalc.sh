8c916caf567b58df97bb04e3fab8a030e04aa061
Salmandjuli <salmandjuli@gmail.com>
2013-06-22 09:49:43 +0700

    Create MiniCalc.sh
    Calculator

---
+++ MiniCalc.sh
@@ -0,0 +1,279 @@
+#!/bin/bash
+opt=""
+gnome=""
+echo
+echo -e "\e[1;32mIni adalah kalkulator mini.\e[0m"
+sleep 2
+echo
+echo -e "\e[1;34mSilahkan tunggu, system sedang melakukan proses.\e[0m"
+sleep 4
+clear
+echo -e "\e[1;36mSystem sudah siap untuk menghitung.\e[0m"
+sleep 1
+echo
+vech1=Identification-$nama......
+clear
+printf "%1.1s\n" $vech1
+sleep 1
+clear
+printf "%1.2s\n" $vech1
+sleep 1
+clear
+printf "%1.3s\n" $vech1
+sleep 1
+clear
+printf "%1.4s\n" $vech1
+sleep 1
+clear
+printf "%1.5s\n" $vech1
+sleep 1
+clear
+printf "%1.6s\n" $vech1
+sleep 1
+clear
+printf "%1.7s\n" $vech1
+sleep 1
+clear
+printf "%1.8s\n" $vech1
+sleep 1
+clear
+printf "%1.9s\n" $vech1
+sleep 1
+clear
+printf "%1.10s\n" $vech1
+sleep 1
+clear
+printf "%1.11s\n" $vech1
+sleep 1
+clear
+printf "%1.12s\n" $vech1
+sleep 1
+clear
+printf "%1.13s\n" $vech1
+sleep 1
+clear
+printf "%1.14s\n" $vech1
+sleep 1
+clear
+printf "%1.15s\n" $vech1
+sleep 1
+clear
+printf "%1.16s\n" $vech1
+sleep 1
+clear
+printf "%1.17s\n" $vech1
+sleep 1
+clear
+printf "%1.18s\n" $vech1
+sleep 1
+clear
+printf "%1.19s\n" $vech1
+sleep 1
+clear
+printf "%1.20s\n" $vech1
+sleep 1
+clear
+printf "%1.21s\n" $vech1
+sleep 1
+clear
+printf "%1.22s\n" $vech1
+sleep 1
+clear
+printf "%1.23s\n" $vech1
+sleep 1
+clear
+printf "%1.24s\n" $vech1
+clear
+printf "%1.25s\n" $vech1
+clear
+printf "%1.26s\n" $vech1
+clear
+printf "%1.27s\n" $vech1
+clear
+printf "%1.28s\n" $vech1
+clear
+printf "%1.29s\n" $vech1
+clear
+echo
+echo -e "\e[1;38mHello, $nama\e[0m"
+sleep 2
+echo -e "\e[1;38mBerhitung dimulai\e[0m"
+sleep 3
+echo
+echo -e "\e[1;38m[+] Sekarang $nama ketik angka pertama yang ingin $nama tambah, kurang, bagi, dan kali, lalu tekan enter :\e[0m"
+read pertama
+sleep 2
+echo
+echo -e "\e[1;38m[+] Tunggu 2 detik, system sedang membuka buku matematika Sekolah Dasar\e[0m"
+sleep 2
+echo
+echo -e "\e[1;38m[+] Sistem operasi apa yang $nama inginkan ?? Operasi penambahan(+), Operasi pengurangan(-), Operasi perkalian(*), Operasi pembagian(/), lalu tekan enter :\e[0m"
+read opt
+sleep 2
+echo
+echo -e "\e[1;38m[+] Sekarang $nama ketik angka kedua yang ingin $nama tambah, kurang, bagi, dan kali, lalu tekan enter :\e[0m"
+read dua
+sleep 3
+echo
+echo -e "\e[1;38m[+] Yang ingin $nama hitung adalah : $pertama $opt $dua =\e[0m"
+sleep 3
+echo
+echo -e "\e[1;38m[+] hasilnya adalah $(( $pertama $opt $dua )) \e[0m"
+sleep 2
+echo -e "\e[1;38m[+]Atau bilangan pecahannya adalah (bila hasilnya pecahan):\e[0m"
+gnome-calculator --solve $pertama$opt$dua
+sleep 5
+clear
+echo
+echo -e "\e[1;32mHidup tak selamanya indah, kadang baik, kadang buruk, kadang naik, kadang terpuruk, kadang mudah, kadang susah.\e[0m"
+sleep 3
+echo
+echo -e "\e[1;32mKehidupan dunia hanya sementara saja, yang merupakan jalan untuk menuju kehidupan yang abadi.\e[0m"
+sleep 3
+echo
+echo -e "\e[1;32mMaka dari itu bersabarlah dalam menjalani hidup ini, sabar didunia akan subur diakhirat kelak.\e[0m"
+sleep 3
+echo
+echo -e "\e[1;32mSpecial thanks to ALLAH SWT yang telah memberi pencerahan kepada saya dalam menyelesaikan project ini,\e[0m"
+sleep 3
+echo
+echo -e "\e[1;32mdan buat semua yang telah mendukung saya serta tentunya buat $nama yang telah menggunakan kalkulator saya dengan bijak ;D\e[0m"
+sleep 3
+echo
+printf "Jangan $nama sering mengatakan\n"
+printf "               _ __   ___  
+              | '_ \ / _ \ 
+              | | | | (_) |
+              |_| |_|\___/  sebelum $nama mencoba sesuatu\n"
+sleep 3
+echo
+printf "Pikiran $nama harus selalu\n"
+printf "               ___  _ __  
+              / _ \| '_ \ 
+             | (_) | | | |
+              \___/|_| |_| agar kehidupan $nama menjadi lebih baik\n"
+sleep 3
+echo
+printf "Ingat pesan saya\n"
+sleep 3
+clear
+vech=Janganlah-suka-meremehkan-orang-lain
+printf "%1.1s\n" $vech
+sleep 1
+clear
+printf "%1.2s\n" $vech
+sleep 1
+clear
+printf "%1.3s\n" $vech
+sleep 1
+clear
+printf "%1.4s\n" $vech
+sleep 1
+clear
+printf "%1.5s\n" $vech
+sleep 1
+clear
+printf "%1.6s\n" $vech
+sleep 1
+clear
+printf "%1.7s\n" $vech
+sleep 1
+clear
+printf "%1.8s\n" $vech
+sleep 1
+clear
+printf "%1.9s\n" $vech
+sleep 1
+clear
+printf "%1.10s\n" $vech
+sleep 1
+clear
+printf "%1.11s\n" $vech
+sleep 1
+clear
+printf "%1.12s\n" $vech
+sleep 1
+clear
+printf "%1.13s\n" $vech
+sleep 1
+clear
+printf "%1.14s\n" $vech
+sleep 1
+clear
+printf "%1.15s\n" $vech
+sleep 1
+clear
+printf "%1.16s\n" $vech
+sleep 1
+clear
+printf "%1.17s\n" $vech
+sleep 1
+clear
+printf "%1.18s\n" $vech
+sleep 1
+clear
+printf "%1.19s\n" $vech
+sleep 1
+clear
+printf "%1.20s\n" $vech
+sleep 1
+clear
+printf "%1.21s\n" $vech
+sleep 1
+clear
+printf "%1.22s\n" $vech
+sleep 1
+clear
+printf "%1.23s\n" $vech
+sleep 1
+clear
+printf "%1.24s\n" $vech
+sleep 1
+clear
+printf "%1.25s\n" $vech
+sleep 1
+clear
+printf "%1.26s\n" $vech
+sleep 1
+clear
+printf "%1.27s\n" $vech
+sleep 1
+clear
+printf "%1.28s\n" $vech
+sleep 1
+clear
+printf "%1.29s\n" $vech
+sleep 1
+clear
+printf "%1.30s\n" $vech
+sleep 1
+clear
+printf "%1.31s\n" $vech
+sleep 1
+clear
+printf "%1.32s\n" $vech
+sleep 1
+clear
+printf "%1.33s\n" $vech
+sleep 1
+clear
+printf "%1.34s\n" $vech
+sleep 1
+clear
+printf "%1.35s\n" $vech
+sleep 1
+clear
+printf "%1.36s\n" $vech
+sleep 1
+echo
+har=Bekerja-Bersabar-Berdoa
+hir=Malas-Pengen-yang-instant-[bukan-mie-instant]-dan-Gak-mau-berdoa
+echo -e "\e[1;37mJika buah dari sukses adalah $har\e[0m"
+sleep 3
+echo
+echo -e "\e[1;37mMaka\e[0m"
+sleep 2
+echo
+echo -e "\e[1;37mBuah dari keterpurukan adalah $hir\e[0m"
+sleep 2
+echo
